3ec421ae54443ebe04f7575ef4a023c76cf0a0f3
Abadul <rrubel00@gmail.com>
2023-11-15 10:34:00 +0600

    hi this Abadul

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+//use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('admin/dashboard', [AdminController::class,'AdminDashbord'])->name('admin.dashboard');
+    Route::get('admin/logout', [AdminController::class,'Adminlogout'])->name('admin.logout');
+
+});
+route::middleware(['auth','role:agent'])->group(function(){
+    //Route::get('agent/dashboard', [AdminController::class,'AgentDashbord'])->name('agent.dashboard');
+    Route::get('agent/dashboard', [AgentController::class,'AgentDashbord'])->name('agent.dashboard');
+
+});
+
+//Route::get('agent/dashboard', [AgentController::class,'AgentDashbord'])->name('agent.dashboard');
+
+require __DIR__.'/auth.php';
